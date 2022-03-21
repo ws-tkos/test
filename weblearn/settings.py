8a44c21c9015fb6132aba90021cdc586352a95bb
ws-tkos <61533933+ws-tkos@users.noreply.github.com>
2022-03-21 16:28:32 +0800

    Update settings.py

--- weblearn/settings.py
+++ weblearn/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysite_db',
         'USER': 'root',
-        'PASSWORD': 'ws1997224',
+        'PASSWORD': 'yourpassword',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
